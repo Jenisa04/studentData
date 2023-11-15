aae38863e480d9a4d59361faacee87e06db732eb
Jenisa <jenisasheth@gmail.com>
2023-11-15 12:12:24 -0800

    validating data, authenticating to another API

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 const csv = require('csv-parser');
+const axios = require('axios');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,59 +11,154 @@ const MAX_RECORDS = 10;
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('file'), (req, res) => {
+const expectedHeaders = [
+  'Student_Id',
+  'First_Name',
+  'Last_Name',
+  'Email',
+  'Upload_Date',
+  'Title_Code',
+  'Percentage',
+];
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+app.post('/upload', upload.single('file'), async (req, res) => {
   try {
-    // Validate input file
+    // input file
     if (!req.file || !req.file.originalname.endsWith('.csv')) {
-      return res.status(400).send('Invalid file format. Please upload a CSV file.');
+      return res.status(400).json({ error: 'Invalid file format. Please upload a CSV file.' });
     }
 
-    // Parse CSV file
+    // empty file
+    if (req.file.size === 0) {
+      return res.status(400).json({ error: 'Uploaded file is empty.' });
+    }
+
+    //header fields checked
+    const fileHeaders = req.file.buffer.toString().split('\n')[0].trim().split(',');
+    console.log("File headers: ", fileHeaders);
+    console.log("Expected headers: ", expectedHeaders);
+    if (!arraysEqual(expectedHeaders, fileHeaders)) {
+      return res.status(400).json({ error: 'Invalid header fields in the CSV file.' });
+    }
+
+    // Parse through CSV file
     const records = [];
+    const validationErrors = [];
     const lines = req.file.buffer.toString().split('\n');
 
-    // Skip the header and start processing from the first data row
     for (let index = 1; index < lines.length && records.length <= MAX_RECORDS; index++) {
-    const line = lines[index];
+      const line = lines[index];
 
-    // Validate number of fields
-    const fields = line.split(',');
-    console.log(line);
-    if (fields.length !== 7) {
-      return res.status(400).json({ error: `Invalid number of columns in row ${index}.` });
+      // no. of fields checked
+      const fields = line.split(',');
+      if (fields.length !== 7) {
+        validationErrors.push(`Invalid number of columns in row ${index}.`);
+        continue;
+      }
+
+      const record = {
+        Student_Id: parseInt(fields[0]),
+        First_Name: fields[1],
+        Last_Name: fields[2],
+        Email: fields[3],
+        Upload_Date: new Date(fields[4]),
+        Title_Code: parseInt(fields[5]),
+        Percentage: parseFloat(fields[6]),
+      };
+
+      // Validating data for each row
+      const recordValidation = isValidRecord(record, index);
+      if (recordValidation !== true) {
+        validationErrors.push(recordValidation);
+        continue;
+      }
+
+      records.push(record);
     }
 
-    const record = {
-      Student_Id: parseInt(fields[0]),
-      First_Name: fields[1],
-      Last_Name: fields[2],
-      Email: fields[3],
-      Upload_Date: new Date(fields[4]),
-      Title_Code: parseInt(fields[5]),
-      Percentage: parseFloat(fields[6]),
-    };
-
-    // Validate data for each row
-    if (!isValidRecord(record)) {
-      return res.status(400).json({ error: `Invalid data in row ${index}.` });
+    // validation errors
+    if (validationErrors.length > 0) {
+      return res.status(400).json({ error: 'Validation errors', validationErrors });
     }
 
-    records.push(record);
+    
+    // Authenticate to another API enabled backend environment
+    const apiEndpoint = 'https://ucdavis-iet.com/sample-endpoint-url';
+
+    try {
+      if (isDevelopment) {
+        // successful response in development
+        console.log('Simulating success in development mode.');
+        const fakeSuccessResponse = { success: true };
+        return res.status(200).json(fakeSuccessResponse);
+      } else {
+        // real API request in production
+        const response = await axios.post(apiEndpoint, { records });
+        await sendEmail('CSV Processing Successful', 'The CSV file has been processed successfully.');
+        return res.status(200).json({ success: 'File processed successfully.', records });
+      }
+    } catch (error) {
+      if (isDevelopment) {
+        // error response in development
+        console.error('Simulating error in development mode.');
+        const fakeErrorResponse = { error: 'Simulated error during API request.' };
+        return res.status(500).json(fakeErrorResponse);
+      } else {
+        // real API in production error
+        await sendEmail('CSV Processing Error', 'There was an error processing the CSV file.');
+        return res.status(500).send('Internal Server Error');
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Internal Server Error');
+  }
+});
+
+function isValidRecord(record, index) {
+  if (isNaN(record.Student_Id) || record.Student_Id <= 0) {
+    return `Invalid Student_Id in row ${index}.`;
   }
 
-    // Processed successfully
-    return res.status(200).json({ success: 'File processed successfully.', records });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).send('Internal Server Error');
-    }
+  if (!isValidEmail(record.Email)) {
+    return `Invalid Email in row ${index}.`;
+  }
+  return true;
+}
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+function arraysEqual(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+}
+
+async function sendEmail(subject, text) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'your_email@gmail.com',
+      pass: 'your_password',
+    },
   });
 
-function isValidRecord(record) {
-  // Implement your validation logic here
-  // For simplicity, let's assume all records are valid
-  
-  return true;
+  const mailOptions = {
+    from: 'xyz@gmail.com',
+    to: 'abc@gmail.com',
+    subject,
+    text,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent:', info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 }
 
 app.listen(PORT, () => {
